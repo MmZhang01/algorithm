71009e5a7a76553764f07bb7310781a261126714
MengZhang <meng.2.zhang@uconn.edu>
2022-06-29 20:16:40 -0400

    second commit

--- DataStructure/src/TwoPointers/Test.java
+++ DataStructure/src/TwoPointers/Test.java
@@ -19,15 +19,39 @@ public class Test {
         System.out.println(result.judgeSquareSum(ob1));
     }
 
+    public static void test3(){
+        String meng = "bridge";
+        TwoPointers result = new TwoPointers();
+//        result.reverseVowels(meng);
+        System.out.println(result.reverseVowels(meng));
+    }
 
+    public static void test4(){
+        String meng = "abceaba";
+        TwoPointers result =new TwoPointers();
+        System.out.println(result.validPalindrome(meng));
+    }
 
+    public static void test5(){
+        int[] nums1 = new int[]{1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = new int[]{2,5,6};
+        int n = 3;
+        TwoPointers result = new TwoPointers();
+        result.merge(nums1,m,nums2,n);
+        System.out.println(Arrays.toString(nums1));
+    }
 
 
 
     public static void main(String[] args){
 //        test1();
-        test2();
+//        test2();
+//        test3();
+//        test4();
+        test5();
     }
+
 
 
 }
--- DataStructure/src/TwoPointers/TwoPointers.java
+++ DataStructure/src/TwoPointers/TwoPointers.java
@@ -1,7 +1,13 @@
 package TwoPointers;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class TwoPointers {
 
+    /*
+    * 167. Two Sum II - Input array is sorted (Easy)
+    * */
     public int[] TwoSum(int[] numbers, int target){
         if (numbers == null){return null;}
         int i =0;
@@ -20,6 +26,9 @@ public class TwoPointers {
         return null;
     }
 
+    /*
+    * 633. Sum of Square Numbers (Easy)
+    * */
     public boolean judgeSquareSum(int target){
         if (target < 0){return false;}
 
@@ -37,5 +46,72 @@ public class TwoPointers {
         return false;
     }
 
+    /**
+     * 345. Reverse Vowels of a String (Easy)
+     */
+    private final static HashSet<Character> vowels = new HashSet<>(
+            Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
 
+    public String reverseVowels(String s){
+        if (s == null) {
+            return null;
+        }
+        int i=0,j=s.length()-1;
+        char[] result = new char[s.length()];
+        while(i<=j){
+            char ci = s.charAt(i);
+            char cj = s.charAt(j);
+            if(!vowels.contains(ci)){
+                result[i++]=ci;
+            }else if (!vowels.contains(cj)){
+                result[j--]=cj;
+            }else{
+                result[i++]=cj;
+                result[j--]=ci;
+            }
+        }
+
+    return new String(result);}
+
+    /*
+    680. Valid Palindrome II (Easy)
+     */
+    private boolean isPalindrome(String s, int i, int j){
+        while(i<j){
+            if (s.charAt(i++)!=s.charAt(j--)){
+                return false;
+            }
+        }return true;
+    }
+    public boolean validPalindrome(String s){
+        for (int i=0,j=s.length()-1;i<j;i++,j--){
+            if(s.charAt(i)!=s.charAt(j)){
+                return(isPalindrome(s,i+1,j)||isPalindrome(s,i,j-1));
+            }
+        }return true;
+    }
+
+    /**
+     * 88. Merge Sorted Array (Easy)
+     */
+    public void merge(int[] nums1,int m, int[] nums2, int n){
+        int index1 = m-1,index2=n-1;
+        int indexMerge = m+n-1;
+        while(index1>=0){
+            if (index1<0){
+                nums1[indexMerge--]=nums2[index2--];
+            }else if(index2<0){
+                nums1[indexMerge--]=nums1[index1--];
+            }else if(nums1[index1]<nums2[index2]){
+                nums1[indexMerge--]=nums2[index2--];
+            }else{
+                nums1[indexMerge--]=nums1[index1--];
+            }
+        }
+    }
+
+    /**
+     * 141. Linked List Cycle (Easy)
+     */
+    
 }
