eacf07c9822add47a9b305da41410b57e521ad64
zhang meng <meng.2.zhang@uconn.edu>
2022-08-16 22:09:50 -0400

    BST

--- DataStructure/src/Tree/BST.java
+++ DataStructure/src/Tree/BST.java
@@ -1,4 +1,20 @@
 package Tree;
 
+
 public class BST {
+    /**
+     * 669. Trim a Binary Search Tree (Easy)
+     */
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if(root== null) return null;
+        if(root.val<low) return trimBST(root.right,low,high);
+        if(root.val>high) return trimBST(root.left,low,high);
+        root.left = trimBST(root.left,low,high);
+        root.right=trimBST(root.right,low,high);
+        return root;
+    }
+
+    /**
+     * 230. Kth Smallest Element in a BST (Medium)
+     */
 }
