2df65bcc73a61a562d162009e75efce105ab1e9c
zhang meng <meng.2.zhang@uconn.edu>
2022-10-03 15:44:05 -0400

    11

--- DataStructure/src/DailyPractice/DailyPractice8.java
+++ DataStructure/src/DailyPractice/DailyPractice8.java
@@ -1,5 +1,24 @@
 package DailyPractice;
 
+import org.junit.Test;
+
 public class DailyPractice8 {
-    
+    @Test
+    public static void main(String[] args){
+        int i=0,j=0;
+//        while(i<10){
+//            i++;
+//            ++j;
+//            System.out.println("i="+i);
+//            System.out.println("j="+j);
+//        }
+        for(i=0;i<10;){
+            int a=i++;
+            int b=++i;
+            System.out.println("a="+a);
+            System.out.println("b="+b);
+        }
+
+    }
+
 }
